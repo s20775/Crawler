e49414a00ae3a420fea62c2f066c0cd21915a937
Algi42 <artur.rzodkiewicz@gmail.com>
2022-03-20 19:57:54 +0100

    Pierwszy commit

--- Crawler/Program.cs
+++ Crawler/Program.cs
@@ -13,30 +13,29 @@ namespace Crawler
             if (args.Length == 0)
                 throw new ArgumentNullException("Brak argumentów");
 
-            string url = args[0];
+            string websiteUrl = args[0];
 
-            if (!(Uri.IsWellFormedUriString(url, UriKind.Absolute)))
+            if (!(Uri.IsWellFormedUriString(websiteUrl, UriKind.Absolute)))
                 throw new ArgumentException("Podaj poprawny adres URL");
 
-            HttpClient httpClient = new();
+            HttpClient httpClient = new HttpClient();
 
             try
             {
-                HttpResponseMessage response = await httpClient.GetAsync(url);
+                HttpResponseMessage response = await httpClient.GetAsync(websiteUrl);
 
 
                 if (response.IsSuccessStatusCode)
                 {
-                    string siteContent = await response.Content.ReadAsStringAsync(); 
-                    String pattern = @"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])";
+                    string content = await response.Content.ReadAsStringAsync(); 
+                    
+                    Regex regex = new Regex(@"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])");
 
-                    Regex regex = new(pattern, RegexOptions.IgnoreCase); 
-
-                    MatchCollection mathCollection = regex.Matches(siteContent);
+                    MatchCollection matchCollection = regex.Matches(content);
 
                     HashSet<string> hashSet = new HashSet<string>();
 
-                    foreach (Match match in mathCollection)
+                    foreach (Match match in matchCollection)
                         hashSet.Add(match.Value);
 
                     if (hashSet.Count >= 1)
